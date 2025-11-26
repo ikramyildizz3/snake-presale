fdfa66b3ca9c71c71ba98dc00881b84576c0710e
Ikram <ikramyildizz3@gmail.com>
2025-11-26 13:20:40 +0300

    restore client redirect to snake-token.html

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    // Root açılınca statik sayfaya gönder
+    window.location.href = "/snake-token.html";
+  }, []);
+
   return null;
 }
