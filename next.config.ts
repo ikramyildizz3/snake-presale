f298cb6cb6f6159ad554c54aae8d178f89ee84bf
Ikram <ikramyildizz3@gmail.com>
2025-11-26 12:55:19 +0300

    add root rewrite to snake-token.html for snaketoken.app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",                 // snaketoken.app/
+        destination: "/snake-token.html", // public içindeki statik HTML
+      },
+    ];
+  },
 };
 
 export default nextConfig;
