d7d22a47789bb887a17fd0b7e074746aef84a144
Ikram <ikramyildizz3@gmail.com>
2025-11-26 12:37:36 +0300

    add vercel rewrite and custom snake favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-
 export default function Home() {
-  useEffect(() => {
-    window.location.href = "/snake-token.html";
-  }, []);
-
   return null;
 }
